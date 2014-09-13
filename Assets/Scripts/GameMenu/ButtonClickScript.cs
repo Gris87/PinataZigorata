74e40cfa0c4309a4342de0345c11405e33d3eb95
Gris87 <Gris87@yandex.ru>
2014-09-13 23:36:27 +0400

    GameMenu

---
+++ Assets/Scripts/GameMenu/ButtonClickScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonClickScript : MonoBehaviour
+{
+	public AudioClip clip;
+
+	private void playSound()
+	{
+		audio.PlayOneShot(clip, 1); // TODO: Options.effectsVolume);
+	}
+
+	public void OnNewGameClicked()
+	{
+		Debug.Log("New game pressed");
+
+		playSound();
+	}
+	
+	public void OnSettingsClicked()
+	{
+		Debug.Log("Settings pressed");
+
+		playSound();
+	}
+
+	public void OnExitClicked()
+	{
+		Debug.Log("Application finished");
+		Application.Quit();
+
+		playSound();
+	}
+}
