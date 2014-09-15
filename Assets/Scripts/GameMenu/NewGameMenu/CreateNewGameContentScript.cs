9d38ced4fdd536b070197df89cdc66e982363bff
Gris87 <Gris87@yandex.ru>
2014-09-15 23:28:56 +0400

    NewGameMenu

---
+++ Assets/Scripts/GameMenu/NewGameMenu/CreateNewGameContentScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreateNewGameContentScript : MonoBehaviour
+{
+	// Use this for initialization
+	void Start()
+	{
+		Object[] levels = Resources.LoadAll("Levels");
+
+		for (int i=0; i<levels.Length; ++i)
+		{
+
+		}
+	}
+}
